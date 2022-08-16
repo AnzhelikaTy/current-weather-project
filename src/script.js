8cd920f5ececc026d7d86ac6494e15d1f304f841
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-08-16 16:51:43 -0400

    updated js for Celsius
    possibility to swith between C° an F° and vice versa

--- src/script.js
+++ src/script.js
@@ -87,10 +87,19 @@ function displayFahrenheitTemperature(event) {
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperature.innerHTML = Math.round(fahrenheitTemperature);
 }
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#current-temperature");
+  temperature.innerHTML = Math.round(celsiusTemperature);
+}
 let celsiusTemperature = null;
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let form = document.querySelector("#button-change-city");
 form.addEventListener("submit", searchCity);
