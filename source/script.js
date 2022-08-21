7efc00d2d47783f7ff1476d7e08fb976aff36532
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-08-20 21:38:25 -0400

    fixed mistake, added img to forecast to HTML

--- source/script.js
+++ source/script.js
@@ -71,6 +71,7 @@ function getForecast(coordinates) {
   let forecastEndPoint = `https://api.openweathermap.org/data/2.5/onecall?`;
   let forecastUrl = `${forecastEndPoint}lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(forecastUrl).then(displayForecast);
+  console.log(forecastUrl);
 }
 //Search engine
 
